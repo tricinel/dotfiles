787567d9273f461fec3a2ba60fcd0effe01f54bb
tricinel <bogdan.freelance@gmail.com>
2014-06-02 18:35:35 +0300

    added npm function to install modeule from git repo, from a specific branch/tag

--- dev.zsh
+++ dev.zsh
@@ -12,3 +12,14 @@ alias gin='gem install --no-ri --no-rdoc'
 
 # Scaffold a new MEAN app using yeoman and generator-angular-fullstack
 alias mean='yo angular-fullstack'
+
+# Install an npm module from a git repo using a specific branch
+# e.g. npm-git myrepo/myproject master -g or npm-git myrepo/myproject master
+function npm-git {
+  if [ "$3" != "-g" ]
+    then
+      npm install git+ssh://git@github.com:$1.git#$2
+    else
+      npm install -g git+ssh://git@github.com:$1.git#$2
+    fi
+}
