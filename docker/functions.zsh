87e430a2bf16170260e4f82b2ebff589671451a8
tricinel <bogdan@blazardesign.com>
2016-05-04 09:01:06 +0100

    Add docker-machine use function

--- docker/functions.zsh
+++ docker/functions.zsh
@@ -1,3 +1,8 @@
 function docker-point {
   eval "$(docker-machine env $1)"
 }
+
+function dcm-use {
+  docker-machine start $@ && eval "$(docker-machine env $@)"
+  echo "Active machine: ${DOCKER_MACHINE_NAME}"
+}
