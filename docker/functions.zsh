922f901892d5c8fa953c0be31b4c45cbe93dd771
tricinel <bogdan@blazardesign.com>
2016-05-04 11:35:26 +0100

    Add docker machine stop function that picks up the active machine automatically

--- docker/functions.zsh
+++ docker/functions.zsh
@@ -6,3 +6,11 @@ function dcm-use {
   docker-machine start $@ && eval "$(docker-machine env $@)"
   echo "Active machine: ${DOCKER_MACHINE_NAME}"
 }
+
+function dcm-stop {
+  if [[ $1 ]]; then
+    docker-machine stop $1
+  else
+    docker-machine stop ${DOCKER_MACHINE_NAME}
+  fi
+}
