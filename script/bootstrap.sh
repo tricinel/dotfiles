c2a89844ef77ce9da10bc409a62afb9810266335
Bogdan Lazar <bogdan@bogdanlazar.com>
2019-02-01 11:11:00 +0100

    Add tig install

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -74,7 +74,10 @@ install_brew_packages () {
 
   # Command-line fuzzy finder
   brew install fzf
-  
+
+  # Tig: text-mode interface for Git
+  brew install tig
+
   # install nvm
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 }
