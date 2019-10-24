147aaf4055d43870d280b84a73ded8962e95b907
Bogdan Lazar <bogdan@bogdanlazar.com>
2019-10-24 11:20:44 +0200

    Add a better git clone, stolen from @jaredpalmer

--- git/functions.zsh
+++ git/functions.zsh
@@ -48,3 +48,54 @@ function gsafemerge() {
     git merge --no-ff --no-commit $1
   fi
 }
+
+# A better git clone
+# clones a repository, cds into it, and opens it in my editor.
+#
+# Shamelessly stolen from https://github.com/jaredpalmer/dotfiles/blob/master/.functions#L63 by @jaredpalmer
+#
+# Note: c is already setup as a shortcut to VSCode. Replace with your own editor if different
+#
+# - arg 1 - url|username|repo remote endpoint, username on github, or name of
+#           repository.
+# - arg 2 - (optional) name of repo
+#
+# usage:
+#   $ clone things
+#     .. git clone git@github.com:addyosmani/things.git things
+#     .. cd things
+#     .. subl .
+#
+#   $ clone yeoman generator
+#     .. git clone git@github.com:yeoman/generator.git generator
+#     .. cd generator
+#     .. subl .
+#
+#   $ clone git@github.com:addyosmani/dotfiles.git
+#     .. git clone git@github.com:addyosmani/dotfiles.git dotfiles
+#     .. cd dots
+#     .. subl .
+
+function clone {
+  # customize username to your own
+  local username="tricinel"
+
+  local url=$1;
+  local repo=$2;
+
+  if [[ ${url:0:4} == 'http' || ${url:0:3} == 'git' ]]
+  then
+    # just clone this thing.
+    repo=$(echo $url | awk -F/ '{print $NF}' | sed -e 's/.git$//');
+  elif [[ -z $repo ]]
+  then
+    # my own stuff.
+    repo=$url;
+    url="git@github.com:$username/$repo";
+  else
+    # not my own, but I know whose it is.
+    url="git@github.com:$url/$repo.git";
+  fi
+
+  git clone $url $repo && cd $repo && c .;
+}
