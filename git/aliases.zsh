294849d8e18691e6e4e736eb362da09d1f0002f5
Bogdan Lazar <bogdan@bogdanlazar.com>
2024-02-14 11:30:01 +0100

    Change some git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,2 +1,5 @@
 # Add untracked, remove deleted, and show status
 alias gad='git add -A && git status -sb'
+
+# Stash the current, then update to the latest, then pop the stash
+alias gpr='git stash --all && git pull --rebase && git stash pop'
--- git/functions.zsh
+++
@@ -1,6 +0,0 @@
-# Stash the current, then update to the latest, then pop the stash
-function gpr {
-  git stash --all
-  git pull --rebase
-  git stash pop
-}
