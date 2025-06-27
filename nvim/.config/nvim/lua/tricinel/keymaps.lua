82040199195666b4e2f662df6eb741320585d6d6
Bogdan Lazar <bogdan@bogdanlazar.com>
2025-06-27 13:32:39 +0200

    Add Twilight notification on toggle

--- nvim/.config/nvim/lua/tricinel/keymaps.lua
+++ nvim/.config/nvim/lua/tricinel/keymaps.lua
@@ -50,7 +50,12 @@ set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
 
 -- Twilight
 set("n", "<leader>ut", function()
-  vim.cmd("Twilight")
+  local twilight = require("twilight")
+  twilight.toggle()
+
+  local is_active = require("twilight.view").enabled
+  local msg = is_active and "Twilight enabled" or "Twilight disabled"
+  vim.notify(msg, vim.log.levels.INFO, { title = "Twilight" })
 end, { desc = "Toggle Twilight" })
 
 -- Source file
