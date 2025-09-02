494047c463bedc472838a991ecf64f0a588087b5
Bogdan Lazar <bogdan@bogdanlazar.com>
2025-09-02 12:17:14 +0200

    Setup biome and css lsp

--- nvim/.config/nvim/lua/tricinel/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/tricinel/plugins/lspconfig.lua
@@ -2,5 +2,26 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    servers = {
+      biome = {
+        settings = {
+          biome = {
+            formatting = {
+              enabled = true,
+            },
+          },
+        },
+      },
+      cssls = {
+        settings = {
+          css = {
+            validate = true,
+            lint = {
+              unknownAtRules = "ignore",
+            },
+          },
+        },
+      },
+    },
   },
 }
