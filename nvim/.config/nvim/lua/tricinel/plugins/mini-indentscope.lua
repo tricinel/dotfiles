dc2053094b7fa7c41212758871a11b0258596cd4
Bogdan Lazar <bogdan@bogdanlazar.com>
2025-09-26 09:47:04 +0200

    mini plugins moved

--- nvim/.config/nvim/lua/tricinel/plugins/mini-indentscope.lua
+++ nvim/.config/nvim/lua/tricinel/plugins/mini-indentscope.lua
@@ -1,5 +1,5 @@
 return {
-  "echasnovski/mini.indentscope",
+  "nvim-mini/mini.indentscope",
   event = "LazyFile",
   opts = {
     symbol = "│",
--- nvim/.config/nvim/lua/tricinel/plugins/mini-surround.lua
+++ nvim/.config/nvim/lua/tricinel/plugins/mini-surround.lua
@@ -1,5 +1,5 @@
 return {
-  "echasnovski/mini.surround",
+  "nvim-mini/mini.surround",
   recommended = true,
   keys = function(_, keys)
     local opts = LazyVim.opts("mini.surround")
