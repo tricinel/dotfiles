fc2a286fdef8dc86e97b42de03e73596f5ff3815
Bogdan Lazar <bogdan@bogdanlazar.com>
2026-06-08 08:50:37 +0200

    Cleanup and add cmd line support in blink

--- nvim/.config/nvim/lua/tricinel/plugins/blink.lua
+++ nvim/.config/nvim/lua/tricinel/plugins/blink.lua
@@ -1,29 +1,38 @@
 vim.pack.add({
-	-- Handles autocompletion menus with LSP integration (single plugin, no dependencies)
-	{ src = "https://github.com/saghen/blink.cmp", version = vim.version.range("^1") },
+  -- Handles autocompletion menus with LSP integration (single plugin, no dependencies)
+  { src = "https://github.com/saghen/blink.cmp", version = vim.version.range("^1") },
 })
 require("blink.cmp").setup({
-	-- Keymap preset: use tab/shift-tab for completion navigation
-	keymap = { preset = "super-tab" },
+  -- Keymap preset: use tab/shift-tab for completion navigation
+  keymap = { preset = "super-tab" },
 
-	-- Appearance settings
-	appearance = {
-		-- Use mono-style nerd font icons
-		nerd_font_variant = "mono",
-	},
+  -- Appearance settings
+  appearance = {
+    -- Use mono-style nerd font icons
+    nerd_font_variant = "mono",
+  },
 
-	-- Completion menu behavior
-	completion = {
-		-- Show documentation automatically when hovering over completion item
-		documentation = { auto_show = true, auto_show_delay_ms = 200 },
-	},
+  -- Completion menu behavior
+  completion = {
+    -- Show documentation automatically when hovering over completion item
+    documentation = { auto_show = true, auto_show_delay_ms = 200 },
+  },
 
-	-- Configure completion sources (where suggestions come from)
-	sources = {
-		-- Use these sources in this priority order
-		default = { "lsp", "path", "snippets", "buffer" },
-	},
+  -- Completion line comletions
+  cmdline = {
+    enabled = true,
+    keymap = { preset = "inherit" },
+    completion = {
+      menu = { auto_show = true },
+    },
+  },
 
-	-- Signature help (shows function parameters as you type)
-	signature = { enabled = true },
+  -- Configure completion sources (where suggestions come from)
+  sources = {
+    -- Use these sources in this priority order
+    default = { "lsp", "buffer", "path", "snippets" },
+  },
+
+  -- Signature help (shows function parameters as you type)
+  signature = { enabled = true },
 })
