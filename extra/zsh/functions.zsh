9ae197023fce31a66b946ba17147b2ba76e5d0ed
Bogdan Lazar <bogdan@bogdanlazar.com>
2026-06-11 09:50:36 +0200

    Cleanup some aliases

--- extra/zsh/functions.zsh
+++ extra/zsh/functions.zsh
@@ -53,18 +53,6 @@ function loc() {
   echo "${fg[blue]}Total${reset_color} lines of code: ${fg[green]}$total${reset_color}"
 }
 
-# List the contents of a directory in a tree-like format
-# using tree from homebrew
-# http://brewformulas.org/Tree
-# This also passes some reasonable defaults
-function lt() {
-  if [ -z "$1" ]; then
-    tree --dirsfirst -I 'bower_components|node_modules'
-  else
-    tree -L $1 --dirsfirst -I 'bower_components|node_modules'
-  fi
-}
-
 # Copy files with progress
 function rcp () {
   rsync -WavP --human-readable --progress $1 $2
