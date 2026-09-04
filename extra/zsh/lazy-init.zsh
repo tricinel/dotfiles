202f4245c14ba326cffbe40126bb2eeca2239b41
Bogdan Lazar <bogdan@bogdanlazar.com>
2026-09-04 12:22:36 +0200

    Add lazy loading for interactive zoxide

--- extra/zsh/lazy-init.zsh
+++ extra/zsh/lazy-init.zsh
@@ -1,9 +1,20 @@
-# lazy-load zoxide on first j use
+# lazy-load zoxide on first j or ji use
 j() {
   if command -v zoxide >/dev/null 2>&1; then
-    unfunction j
+    unfunction j ji
     eval "$(zoxide init zsh --cmd j)"
     j "$@"
+  else
+    print -u2 -- "zoxide not found"
+    return 127
+  fi
+}
+
+ji() {
+  if command -v zoxide >/dev/null 2>&1; then
+    unfunction j ji
+    eval "$(zoxide init zsh --cmd j)"
+    ji "$@"
   else
     print -u2 -- "zoxide not found"
     return 127
